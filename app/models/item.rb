75c8fdaa97f1ab7f9ac3100d77ae49e2a1b90567
Samy Amar <samy.amar.paris@gmail.com>
2016-02-15 18:46:25 +0100

    inclusion done

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,5 @@ DRUGS = ["cocaïne", "weed", "MDMA", "Cartes Pokémon"]
 class Item < ActiveRecord::Base
   belongs_to :user
   has_many :deals
-  validates :name, inclusion { in: DRUGS }
+  validates :name, inclusion: { in: DRUGS }
 end
