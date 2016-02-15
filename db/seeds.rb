2478ed2183e4511006855326b5aa633419b2fc70
massimo ercolani <massimo.ercolani28@gmail.com>
2016-02-15 18:41:35 +0100

    created seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+100.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    photo: Faker::Avatar.image,
+    address: Faker::Address.street_address,
+    zipcode: Faker::Address.zip_code,
+    city: Faker::Address,
+    description: Faker::Hipster.paragraph)
+  user.save
+end
+
+150.times do
+  item = Item.new(
+    name: DRUGS[rand(DRUGS.length - 1)],
+    in_stock: rand > 0.8 ? true : false,
+    price: (rand * (300 - 15) + 15).round(2),
+    user: User.all.sample)
+  item.save
+end
+
+200.times do
+  deal = Deal.new(
+    user: User.all.sample,
+    total_price: (rand * (1_500 - 15) + 15).round(2),
+    rating: (0..5).to_a.sample,
+    item: Item.all.sample,
+    status: %w(Pending Validated Cancelled).sample)
+  deal.save
+end
