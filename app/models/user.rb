164b5abc292b6038986b03a6aee447bb5e301ca1
Samy Amar <samy.amar.paris@gmail.com>
2016-02-15 17:20:42 +0100

    tables and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :items
+  has_many :deals, through: :items
+  has_many :orders, class_name:'Deal'
 end
