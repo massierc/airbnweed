3c39181ca05e5c1f020f86f01521313c8e9fe9e3
massimo ercolani <massimo.ercolani28@gmail.com>
2016-02-15 19:18:06 +0100

    corrected user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,8 @@
 
 100.times do
   user = User.new(
+    email: Faker::Internet.email,
+    password: "dreamteam",
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     photo: Faker::Avatar.image,
