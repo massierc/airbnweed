974dad7993f0fc6af5fe9475783546be6db18d2c
massimo ercolani <massimo.ercolani28@gmail.com>
2016-02-16 16:16:16 +0100

    filtering logic applied

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @dealers = User.select { |user| user.items != [] }
+    @all_dealers = User.select { |user| user.items != [] }
+    @filtered_dealers = []
+    Item.where(name: "#{params[:drug]}").map(&:user_id).each { |user| @filtered_dealers << User.find(user) }
+    User.where(city: "#{params[:city]}").each { |user| @filtered_dealers << user }
   end
 
   def show
@@ -21,3 +24,5 @@ class UsersController < ApplicationController
   def destroy
   end
 end
+
+
