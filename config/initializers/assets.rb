9f3b21b97536128758cd9e73de844314655ed79f
Samy Amar <samy.amar.paris@gmail.com>
2016-02-16 17:23:20 +0100

    finished rough show view, without crash-testing user photo

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( airbnweed.png )
+Rails.application.config.assets.precompile += %w( airbnweed.png weed.jpg MDMA.jpg cocaïne.jpg )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
