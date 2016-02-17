1d7580d4640c6d98c8a03bece8e94ef74e1c4e71
Samy Amar <samy.amar.paris@gmail.com>
2016-02-17 12:12:24 +0100

    Self user page now complete, added header images and intelligence for all DRUGS constant

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
   def create
+    @item = Item.new(item_params)
+    @item.in_stock = true
+    @item.user = current_user
+    @item.save
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price)
   end
 end
