082ab57fa21087ee9df73dc8b754f100f57f751c
massimo ercolani <massimo.ercolani28@gmail.com>
2016-02-17 13:18:15 +0100

    dealers page style

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-  def truncate(s, max = 100, elided = ' ...')
+  def truncate(s, max = 40, elided = ' ...')
     s.match(/(.{1,#{max}})(?:\s|\z)/)[1].tap do |res|
       res << elided unless res.length == s.length
     end
