5d656ce55e69ce4349b3516bb4a787edd5cd92a7
Quitterie Mathelin Moreaux <q.mathelin@gmail.com>
2016-02-17 11:04:10 +0100

    facebook connect added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
   has_many :items
   has_many :deals, through: :items
   has_many :orders, class_name:'Deal'
@@ -12,4 +11,19 @@ class User < ActiveRecord::Base
   def name
     return self.first_name + " " + self.last_name
   end
+
+  def self.find_for_facebook_oauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]  # Fake password for validation
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.picture = auth.info.image
+      user.token = auth.credentials.token
+      user.token_expiry = Time.at(auth.credentials.expires_at)
+    end
+  end
+
 end
