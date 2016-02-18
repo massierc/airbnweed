7dc659b05d74f1c17348dd9a13aa2bef28b6c0cc
Quitterie Mathelin Moreaux <q.mathelin@gmail.com>
2016-02-18 11:55:26 +0100

    added my code to this branch for edit page redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,37 @@
 class ApplicationController < ActionController::Base
+  before_action :store_location
   before_action :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
   protect_from_forgery with: :exception
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    return unless request.get?
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath
+    end
+  end
+
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.sign_in_count < 2
       edit_user_path(resource_or_scope)
     else
-      root_path
+      session[:previous_url] || root_path
     end
   end
+
+  def after_update_path_for(resource_or_scope)
+    session[:previous_url] || root_path
+  end
+  def after_sign_out_path_for(resource_or_scope)
+    session[:previous_url] || root_path
+  end
+
 end
