3a7e57c3e220677142afd4829322fb7878fa99b0
Samy Amar <samy.amar.paris@gmail.com>
2016-02-18 14:15:54 +0100

    added footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+  layout 'application', except: :home
+  layout 'application_without_footer', only: :home
 
   def home
     @navbar_class_for_home = 'navbar-wagon-fixed'
